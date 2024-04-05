432649471b62447d160d3399c83b6413dcc42170
Geraldine <gnadela@gmail.com>
2024-04-05 13:14:50 +0200

    requirements

--- streamlit/spredict.py
+++ streamlit/spredict.py
@@ -2,7 +2,6 @@ import pickle
 import pandas as pd
 import os
 import numpy as np
-import xgboost
 
 model_file_path = "C:\\Users\\gnade\\OneDrive\\Desktop\\PythonProjects\\immo-eliza-deployment\\streamlit\\trained_model.pkl"
 
